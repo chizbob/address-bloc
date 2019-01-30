070b01c3010b75a109fa6fc7e77856a9d6f03783
Soo Kim <chizbob@gmail.com>
2019-01-29 16:02:45 -0800

    CP3-DB completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,21 +1,15 @@
 const MenuController = require("../controllers/MenuController");
 
 // #1
-describe("MenuController", () => {
-  beforeEach(() => {
-    this.menu = new MenuController();
-  });
-
-  describe("#getContactCount()", () => {
-
-// #2
-    it("should return 0 when no contacts are in the book", () => {
-       expect(this.menu.getContactCount()).toBe(0);
+  describe("MenuController", () => {
+    beforeEach(() => {
+      this.menu = new MenuController();
     });
 
-    it("should return 1 when there is exactly one contact in the book", () => {
-          this.menu.contacts.push("Bob");
-          expect(this.menu.getContactCount()).toBe(1)
-    });
-  });
+// #3 remindMe
+  describe("#remindMe()", () => {
+    it("should return 'Learning is a life-long pursuit'", () => {
+       expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+    })
+  })
 });
